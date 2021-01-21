b5ce62f821984348cb42734d329b4364f5f00ae3
Johnny Bui <typngeek@gmail.com>
2021-01-21 16:03:23 -0500

    add port to env file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import server from './app';
 
-const PORT = process.env.PORT || 4200;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
   console.log(`🚀 Congregate Backend running on http://localhost:${PORT}`);
