92448b7f446a7994648a1de64f82ea634d53ac10
Johnny Bui <typngeek@gmail.com>
2021-01-27 17:46:47 -0500

    add collision detection for gameid

--- src/api/generateGameID.ts
+++ src/api/generateGameID.ts
@@ -3,6 +3,7 @@ import { getRandomAdjective, getRandomNoun } from '../helpers/randomWords';
 import Game from '../models/Game';
 import { ServerLogger } from '../logger';
 import express from 'express';
+import { nextTick } from 'process';
 
 const app = express.Router();
 
@@ -12,22 +13,34 @@ export function getRandomGameID() {
   );
 }
 
-app.get('/getUniqueGameID', async (req, res) => {
-  var gameID = 'RubberyExaltedWolf';
+app.get('/getUniqueGameID', async (req, res, next) => {
+  var gameID = getRandomGameID();
 
-  var result = await Game.findOne({ gameID });
-  var numAttempts = 1;
-  while (result && numAttempts < 100) {
-    var gameID = getRandomGameID();
-    result = await Game.findOne({ gameID });
-    numAttempts++;
+  if (process.env.NODE_ENV === 'test') {
+    // don't check for collisions, just return
+    return res.json({ gameID });
   }
 
-  if (numAttempts === 100) {
-    ServerLogger.warn('GameID exhaustion, required 100 tries to find Game ID');
-  }
+  try {
+    var result = await Game.findOne({ gameID });
+    var numAttempts = 1;
+    while (result && numAttempts < 100) {
+      gameID = getRandomGameID();
+      result = await Game.findOne({ gameID });
+      numAttempts++;
+    }
+    if (numAttempts === 100) {
+      ServerLogger.warn(
+        'GameID exhaustion, required 100 tries to find Game ID'
+      );
+      return res.json({ error: 'Unable to get GameID' });
+    }
 
-  return res.json({ gameID });
+    return res.json({ gameID });
+  } catch (err) {
+    ServerLogger.error(err);
+    next(err);
+  }
 });
 
 export default app;
