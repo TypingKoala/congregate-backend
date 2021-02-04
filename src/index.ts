73fcb41c1164e849915586ffa3b6eb7806589db9
Johnny Bui <typngeek@gmail.com>
2021-02-04 14:51:10 -0500

    parse port number

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import server from './app';
 
-const PORT = process.env.PORT;
+const PORT = parseInt(process.env.PORT!);
 
 server.listen(PORT, () => {
   console.log(`🚀 Congregate Backend running on http://localhost:${PORT}`);
